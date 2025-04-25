629799b49dd5315b7e46d9b2c50fecfac92ea7bc
eliasmr98 <eliasmartinrivas@gmail.com>
2025-04-25 18:03:24 +0200

    feat: implement routing and layout structure with multiple pages

--- src/ReactGPT.tsx
+++ src/ReactGPT.tsx
@@ -1,5 +1,6 @@
+import { RouterProvider } from 'react-router';
+import { router } from './presentation/router/router';
+
 export const ReactGPT = () => {
-  return (
-    <h1 className="fade-in text-3xl font-bold underline">ReactGPT</h1>
-  )
-}
+  return <RouterProvider router={router} />;
+};
